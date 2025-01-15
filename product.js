07b53f415724657bd6b697b100d5085cc1dab4d8
riyanXkiller <riyanpmuhammed@gmail.com>
2025-01-15 18:47:26 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,63 @@
+const products=[
+    {
+      id : 1,
+      name : 'Camel high ankle boot for Men',
+      image : 'products/FGC0H9050161A_010_0.webp',
+      rate : 6495
+    },
+    {
+      id : 2,
+      name : 'White Fabric Luxury Sports Shoes',
+      image : 'products/FGC0H40S9632A_044_0.webp',
+      rate : 4497
+    },
+    {
+      id : 3,
+      name : 'Camel Boot for Men',
+      image : 'products/FGC0D4050181A_010_0.webp',
+      rate : 6995
+    },
+    {
+      id : 4,
+      name : 'Black/Grey Sports Shoe for men',
+      image : 'products/FGC0EM0S0152A_B34_0.webp',
+      rate : 4797
+    },
+    {
+      id : 5,
+      name : 'Blue Sneakers for men',
+      image : 'products/FGC0H40S7552A_005_0.webp',
+      rate : 3897
+    },
+    {
+      id : 6,
+      name : 'Maroon canvas shoes for women',
+      image : 'products/FLC0310S2612A_026_0.webp',
+      rate : 3746
+    },
+    {
+      id : 7,
+      name : 'Desert Rose sneakers for women',
+      image : 'products/FLC0AX032671A_D02_0.webp',
+      rate : 2997
+    },
+    {
+      id : 8,
+      name : 'Blue slip on shoes',
+      image : 'products/FGC0H40S7542A_005_0.webp',
+      rate : 3597
+    },
+    {
+      id : 9,
+      name : 'Lpink ballerina for women',
+      image : 'products/FLC0B3044201A_065_0.webp',
+      rate : 3995
+    },
+    {
+      id : 10,
+      name : 'Brown Canvas shoes for women',
+      image : 'products/FLC0310S2602A_I50_0.webp',
+      rate : 3371
+    },
+  ]
+  
